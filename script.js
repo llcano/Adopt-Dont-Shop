0feb3acaef022abffd6e9cab39b78067e8367712
llcano <llcano@mymail.mapua.edu.ph>
2026-03-16 19:42:36 +0800

    script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function upDate(previewPic) {
     display.innerHTML = previewPic.alt;
     
     // Update Background Image
+    // Ensure there are no spaces or special characters breaking the URL string
     display.style.backgroundImage = "url('" + previewPic.src + "')";
 }
 
 function unDo() {
     const display = document.getElementById('image');
     
-    // Reset background and text
-    // Setting backgroundImage to 'none' is the standard way to clear it
+    // Reset background to none and restore default text
     display.style.backgroundImage = "none";
     display.innerHTML = "Hover over an image below to display here.";
 }
 
-// Defining the missing functions to prevent console errors
+// These must be defined because they are called in your HTML
 function onFocus(previewPic) {
     upDate(previewPic);
 }
